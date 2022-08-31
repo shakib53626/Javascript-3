24a22103aaf07d8d2ff074c438290d8042789646
shakib53626 <107703379+shakib53626@users.noreply.github.com>
2022-08-31 18:08:14 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -180,5 +180,131 @@ function love(){
 
 
 
-let btnRef = document.querySelectorAll("button-option");
+let btnRef = document.querySelectorAll(".button-option");
 let winningPattern=[[0,1,2], [ 0,3,6], [2,5,8], [6,7,8], [3,4,5], [1,4,7], [0,4,8],[2,4,8]];
+
+let xTurn =true;
+let count = 0;  
+
+
+btnRef.forEach((element)=>{
+    element.addEventListener("click",() =>{
+        if(xTurn){
+            xTurn=false;
+            element.innerText="x";
+            element.disabled = true;
+        }
+        else{
+            xTurn = true;
+            element.innerText="o";
+            element.disabled = true;
+        }
+    })
+})
+
+
+
+function updateClock(){
+            // Time and date object......
+        var date = new Date();
+        var day = date.getDay();
+        var month = date.getMonth();
+        var correntDate = date.getDate();
+        var year = date.getFullYear();
+        var hour = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+        document.getElementById('priods').innerText = 'AM';
+        if(hour == 0){
+            hour = 12;
+        }
+        if(hour > 12){
+            hour = hour -12;
+            document.getElementById('priods').innerText = 'PM';
+
+        }
+
+        Number.prototype.pad = function(digits){
+            for(var n = this.toString(); n.length<digits; n= 0+n);
+            return n;
+        }
+
+
+        if(day == 0){
+            document.getElementById('dayname').innerText = 'Sunday';
+        }
+        else if(day == 1){
+            document.getElementById('dayname').innerText = 'Monday';
+        }
+        else if(day == 2){
+            document.getElementById('dayname').innerText = 'Tuesday';
+        }
+        else if(day == 3){
+            document.getElementById('dayname').innerText = 'Wednesday';
+        }
+        else if(day == 4){
+            document.getElementById('dayname').innerText = 'Thursday';
+        }
+        else if(day == 5){
+            document.getElementById('dayname').innerText = 'Friday';
+        }
+        else{
+            document.getElementById('dayname').innerText = 'Saterday';
+        }
+
+        // month....
+
+
+        if(month == 0){
+            document.getElementById('month').innerText = 'January';
+        }
+        else if(month == 1){
+            document.getElementById('month').innerText = 'February';
+        }
+        else if(month == 2){
+            document.getElementById('month').innerText = 'March';
+        }
+        else if(month == 3){
+            document.getElementById('month').innerText = 'April';
+        }
+        else if(month == 4){
+            document.getElementById('month').innerText = 'May';
+        }
+        else if(month == 5){
+            document.getElementById('month').innerText = 'June';
+        }
+        else if(month == 6){
+            document.getElementById('month').innerText = 'July';
+        }
+        else if(month == 7){
+            document.getElementById('month').innerText = 'August';
+        }
+        else if(month == 8){
+            document.getElementById('month').innerText = 'September';
+        }
+        else if(month == 9){
+            document.getElementById('month').innerText = 'October';
+        }
+        else if(month == 10){
+            document.getElementById('month').innerText = 'November';
+        }
+        else{
+            document.getElementById('month').innerText = 'December';
+        }
+
+
+        document.getElementById('daynum').innerText = correntDate.pad(2);
+        document.getElementById('year').innerText = year;
+
+
+
+        document.getElementById('hour').innerText = hour.pad(2);
+        document.getElementById('minutes').innerText = minutes.pad(2);
+        document.getElementById('seconds').innerText = seconds.pad(2);
+}
+
+
+function initClock(){
+    updateClock();
+    window.setInterval("updateClock()", 1);
+}
